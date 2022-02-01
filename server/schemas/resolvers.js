8dd40cbe3d3be86f429a9edd886c01e0ce2f77ff
Ben H <deepfreespace@gmail.com>
2022-02-01 14:59:00 -0700

    Mutation routes complete w/ authentication

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 // Imports
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -25,6 +26,23 @@ const resolvers = {
                 .select('-__v -password')
                 .populate('friends')
                 .populate('thoughts');
+        },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials!');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials!');
+            }
+            return user;
         },
     },
 };
