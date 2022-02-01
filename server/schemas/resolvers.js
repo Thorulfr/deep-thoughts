6da8fa5516458d6b3e526033f2f672eb4501017c
Ben H <deepfreespace@gmail.com>
2022-01-31 18:33:57 -0700

    GraphQL...set up for good?

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,30 @@
+// Imports
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        // Find all thoughts (by a specific user, if specified)
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        // Find one thought by ID
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        // Find all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // Find one user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         },
     },
 };
