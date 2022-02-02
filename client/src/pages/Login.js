c188554664b02abf83a36efaffe73026a6e90d95
Ben H <deepfreespace@gmail.com>
2022-02-02 14:49:44 -0700

    Login and signup functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,63 +1,76 @@
+// Imports
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+    const [formState, setFormState] = useState({ email: '', password: '' });
 
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
+    const [login, { error }] = useMutation(LOGIN_USER);
 
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
-  };
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+            console.log(data);
+        } catch (e) {
+            console.error(e);
+        }
+        // clear form values
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
 
-  return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Login</h4>
-          <div className='card-body'>
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main className="flex-row justify-center mb-4">
+            <div className="col-12 col-md-6">
+                <div className="card">
+                    <h4 className="card-header">Login</h4>
+                    <div className="card-body">
+                        <form onSubmit={handleFormSubmit}>
+                            <input
+                                className="form-input"
+                                placeholder="Your email"
+                                name="email"
+                                type="email"
+                                id="email"
+                                value={formState.email}
+                                onChange={handleChange}
+                            />
+                            <input
+                                className="form-input"
+                                placeholder="******"
+                                name="password"
+                                type="password"
+                                id="password"
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
+                            <button className="btn d-block w-100" type="submit">
+                                Submit
+                            </button>
+                        </form>
+                        {error && <div>Login failed</div>}
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Login;
