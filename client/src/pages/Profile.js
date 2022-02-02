3326cc322ce6d196c12f040b15571169563ad389
Ben H <deepfreespace@gmail.com>
2022-02-01 22:40:38 -0700

    Profile pages complete

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,50 @@
+// Imports
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+// Import components and pages
+import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
 
 const Profile = () => {
-  return (
-    <div>
-      <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {/* Viewing <usernames>'s profile. */}
-        </h2>
-      </div>
+    const { username: userParam } = useParams();
+    const { loading, data } = useQuery(QUERY_USER, {
+        variables: { username: userParam },
+    });
 
-      <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
+    const user = data?.user || {};
 
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
-      </div>
-    </div>
-  );
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <div className="flex-row mb-3">
+                <h2 className="bg-dark text-secondary p-3 display-inline-block">
+                    Viewing {user.username}'s profile.
+                </h2>
+            </div>
+
+            <div className="flex-row justify-space-between mb-3">
+                <div className="col-12 mb-3 col-lg-8">
+                    <ThoughtList
+                        thoughts={user.thoughts}
+                        title={`${user.username}'s thoughts...`}
+                    />
+                </div>
+                <div className="col-12 mb-3 col-lg-3">
+                    <FriendList
+                        username={user.username}
+                        friendCount={user.friendCount}
+                        friends={user.friends}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
