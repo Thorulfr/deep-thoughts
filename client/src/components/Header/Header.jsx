1b8a3cd88b9d84fef55667df516ab256763758b1
Ben H <deepfreespace@gmail.com>
2022-02-02 16:29:50 -0700

    Logout and personalized nav

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
 // Imports
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
     return (
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
             <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,8 +16,19 @@ const Header = () => {
                     <h1>Deep Thoughts</h1>
                 </Link>
                 <nav className="text-center">
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Sign Up</Link>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link to="/profile">Me</Link>
+                            <a href="/" onClick={logout}>
+                                Log Out
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login">Log In</Link>
+                            <Link to="/signup">Sign Up</Link>
+                        </>
+                    )}
                 </nav>
             </div>
         </header>
